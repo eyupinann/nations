d111587855f64af8141eea6a16e8a7db2896c3a8
Deniz Eyüp İnan <53041708+denizsinan@users.noreply.github.com>
2023-08-18 10:36:14 +0300

    update package.json

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-const NestFactory = require('@nestjs/core');
-const SwaggerModule = require('@nestjs/swagger');
-const DocumentBuilder = require('@nestjs/swagger');
-const AppModule = require('./app.module');
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
